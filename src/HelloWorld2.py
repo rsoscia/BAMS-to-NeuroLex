70e0197c000b911907a814d5ac7fbf72142e7114
rsoscia <ryansoscia@rsoscia-res.dynamic.ucsd.edu>
2013-09-20 12:11:40 -0700

    retrieving definition from birnlex_1489 in neurolex

--- src/HelloWorld2.py
+++ src/HelloWorld2.py
@@ -12,26 +12,28 @@ plugin.register(
 #Get a Graph object
 g = rdflib.Graph()
 # pull in an RDF document from NeuroLex, parse, and store.
+#result = g.parse("http://neurolex.org/wiki/Special:ExportRDF/birnlex_1489", format="application/rdf+xml")
+#result = g.parse("/Users/ryansoscia/BAMS-to-NeuroLex/Data/bams_ontology_2013-07-10_03-20-00.xml", format="application/rdf+xml")
+
 result = g.parse("http://neurolex.org/wiki/Special:ExportRDF/birnlex_1489", format="application/rdf+xml")
+
 
 #Ask a specific question about that RDF document
 
 qres = g.query(
-    """SELECT DISTINCT ?x ?axonl
+    """SELECT DISTINCT ?x
        WHERE {
-          ?x property:Id "nifext_128"^^xsd:string . ?x property:AxonLength ?axonl
+          ?a property:Definition "birnlex_1489"^^xsd:string ?x.
     	}""",
     initNs=dict(
-        property=rdflib.Namespace("http://neurolex.org/wiki/Special:URIResolver/nifext_128"),
+        property=rdflib.Namespace("http://neurolex.org/wiki/Special:URIResolver/birnlex_1489"),
         wiki=rdflib.Namespace("http://neurolex.org/wiki/Special:URIResolver/")))
+
+
+#troubleshooting the problem.. trying to find the root
+#print("root is %s" % root[qres.result[:0]])
+#initBindings={'x' : wiki}
 
 
 #Gives the first property
 print("Definition: %s" % qres.result[0])
-#says the index is out of range
-
-#Gives every property
-print("Definition: %s" % qres.result[:0])
-
-
-
