fb5dcebc95e96d02bb356a3b1619921b337e7d1c
Stephen Larson <stephen.larson@gmail.com>
2013-09-23 12:07:27 -0700

    Formatting nicely

--- src/Unzip_BAMS_Example.py
+++ src/Unzip_BAMS_Example.py
@@ -2,13 +2,17 @@ import zipfile
 from StringIO import StringIO
 
 zipdata = StringIO()
+
 # open the file using a relative path
 r = open("../Data/BAMS1.zip")
+
 # zipdata is a buffer holding the contents of the zip file in memory
 zipdata.write(r.read())
+
 #myzipfile opens the contents of the zip file as an object that knows how
 # to unzip
 myzipfile = zipfile.ZipFile(zipdata)
+
 #grab the contents out of myzipfile by name
 foofile = myzipfile.open('bams_ontology_2013-07-10_03-20-00.xml')
 
