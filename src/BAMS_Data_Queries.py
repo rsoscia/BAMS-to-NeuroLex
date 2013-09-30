7b50b01a16f8c685a54c59213ed8a20674700528
rsoscia <ryansoscia@rsoscia-res.resnet.ucsd.edu>
2013-09-29 18:02:11 -0700

    Basal Ganglia Extraction

--- src/BAMS_Data_Queries.py
+++ src/BAMS_Data_Queries.py
@@ -156,8 +156,19 @@ print(qres.result[5])
   		#File "<stdin>", line 1, in <module>
 		#IndexError: list index out of range
 
+###########################################
+###########################################
+###########################################
+Extension of above queries:
+prefix BAMS: <http://brancusi1.usc.edu/RDF/>
+qres = g.query(
+     """SELECT ?subject ?predicate ?object
+       WHERE {
+          ?subject ?predicate ?object.
+    	} LIMIT 5""")
+print("printing results")
+print(qres.result[0])
 	
-
 
 ##########################################
 EXAMPLE STEPHEN AND I WENT OVER:
@@ -193,11 +204,121 @@ qres = g.query(
     	} LIMIT 5""")
 print("printing results")
 print(qres.result[0])
+	#Returns the following:
+	#(rdflib.term.BNode('N35f13b7fbc4e4b80a47e4bf277acd530'), 
+	#rdflib.term.URIRef(u'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), 
+	#rdflib.term.URIRef(u'file:///anchor'))
+print(qres.result[1])
+	#Returns the following:
+	#(rdflib.term.BNode('N35f13b7fbc4e4b80a47e4bf277acd530'), 
+	#rdflib.term.URIRef(u'http://www.w3.org/1999/xlinktype'), 
+	#rdflib.term.Literal(u'simple'))
+print(qres.result[2])
+	#Returns the following:
+	#(rdflib.term.BNode('N35f13b7fbc4e4b80a47e4bf277acd530'), 
+	#rdflib.term.URIRef(u'http://www.w3.org/1999/xlinkhref'), 
+	#rdflib.term.Literal(u'http://brancusi1.usc.edu/thesaurus/definition/bilateral-symmetry/'))
+print(qres.result[3])
+	#Returns the following:
+	#(rdflib.term.BNode('Ne30bfc8d40c54a339e2e5fc857c4f4a4'), 
+	#rdflib.term.URIRef(u'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), 
+	#rdflib.term.URIRef(u'file:///anchor'))
+print(qres.result[4])
+	#Returns the following:
+	#(rdflib.term.BNode('Ne30bfc8d40c54a339e2e5fc857c4f4a4'), 
+	#rdflib.term.URIRef(u'http://www.w3.org/1999/xlinktype'), 
+	#rdflib.term.Literal(u'simple'))
 
-
-
-
+print(qres.result[5])
+	#Returns the following:
+	#Traceback (most recent call last):
+  	#File "<stdin>", line 1, in <module>
+	#IndexError: list index out of range
+print(qres.result[0][0])
+	#Returns the following:
+	#N35f13b7fbc4e4b80a47e4bf277acd530
+print(qres.result[0][1])
+	#Returns the following:
+	#http://www.w3.org/1999/02/22-rdf-syntax-ns#type
+print(qres.result[0][2])
+	#Returns the following:
+	#file:///anchor
+print(qres.result[0][3])
+	#Returns the following:
+	#Traceback (most recent call last):
+  	#File "<stdin>", line 1, in <module>
+	#IndexError: tuple index out of range
+print(qres.result[0][4])
+	#Returns the following:
+	#Traceback (most recent call last):
+  	#File "<stdin>", line 1, in <module>
+	#IndexError: tuple index out of range
+print(qres.result[0][5])
+	#Returns the following:
+	#Traceback (most recent call last):
+  	#File "<stdin>", line 1, in <module>
+	#IndexError: tuple index out of range
+print(qres.result[1][1])
+	#Returns the following:
+	#http://www.w3.org/1999/xlinktype
+print(qres.result[2][1])
+	#Returns the following:
+	#http://www.w3.org/1999/xlinkhref
+print(qres.result[3][1])
+	#Returns the following:
+	#http://www.w3.org/1999/02/22-rdf-syntax-ns#type
+print(qres.result[4][1])
+	#Returns the following:
+	#http://www.w3.org/1999/xlinktype
+#print(qres.result[5][1])
+	#Traceback (most recent call last):
+ 	#File "<stdin>", line 1, in <module>
+	#IndexError: list index out of range
 #############################################
+#Modifying the limit size, then printing the first 20 triples
+prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+prefix BAMS: <http://brancusi1.usc.edu/RDF/>
+qres = g.query(
+     """SELECT DISTINCT ?x ?def
+       WHERE {
+          ?x BAMS:Definition "Basal ganglia"^^xsd:string .
+          ?x BAMS:Definition ?def.
+    	} LIMIT 20""")
+print("printing results")
+print(qres.result[0])
+print(qres.result[1])
+print(qres.result[2])
+print(qres.result[3])
+print(qres.result[4])
+print(qres.result[5])
+print(qres.result[6])
+print(qres.result[7])
+print(qres.result[8])
+print(qres.result[9])
+print(qres.result[10])
+print(qres.result[11])
+print(qres.result[12])
+print(qres.result[13])
+print(qres.result[14])
+print(qres.result[15])
+print(qres.result[16])
+print(qres.result[17])
+print(qres.result[18])
+print(qres.result[19])
+print(qres.result[20])
+
+Says the index is out of range when we get to printing the results at index = 4,
+Maybe this means there are only 5 types of basal ganglia defined by the BAMS thesaurus...
+Going to website to double check.. pretty sure i saw this before though.
+
+http://brancusi1.usc.edu/thesaurus/list/by_initial/B/
+
+the above link supports my observation, because there were 5 terms with the strings "basal ganglia" in the front
+
+
+
+
+
 #Ask a specific question about that RDF document
 qres = g.query(
 	prefix bams: <http://brancusi1.usc.edu/RDF/>
