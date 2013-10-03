afa19ed8eab4b0618afdf196a624baf5b6c4c0f7
rsoscia <ryansoscia@rsoscia-res.dynamic.ucsd.edu>
2013-10-03 13:22:19 -0700

    Extracting BAMS Data

--- src/BAMS_Data_Queries.py
+++ src/BAMS_Data_Queries.py
@@ -204,6 +204,11 @@ qres = g.query(
     	} LIMIT 5""")
 print("printing results")
 print(qres.result[0])
+
+#for res in result["results"]["bindings"] :
+#    print res['label']['value']
+
+
 	#Returns the following:
 	#(rdflib.term.BNode('N35f13b7fbc4e4b80a47e4bf277acd530'), 
 	#rdflib.term.URIRef(u'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'), 
@@ -316,9 +321,209 @@ http://brancusi1.usc.edu/thesaurus/list/by_initial/B/
 the above link supports my observation, because there were 5 terms with the strings "basal ganglia" in the front
 
 
+#### Regardless of what i think... the above query sucks ####
+
+#########################################################################################
+#########################################################################################
+
+#Link to the basal ganglia page:
+#http://brancusi1.usc.edu/thesaurus/definition/basal-ganglia-4/
+#prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+#prefix BAMS: <http://brancusi1.usc.edu/RDF/>
+#prefix BAMS: <http://brancusi1.usc.edu/thesaurus/>
+#Try this: http://brancusi1.usc.edu/thesaurus/list/by_initial/B/
+http://brancusi1.usc.edu/RDF/thesaurus
+<http://brancusi1.usc.edu/thesaurus/list/by_initial/B/>
+<http://brancusi1.usc.edu/thesaurus/definition/basal-ganglia-4/>
+
+prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+prefix BAMS: <http://brancusi1.usc.edu/RDF/thesaurus>
 
 
+#Try this link:
+#<http://brancusi1.usc.edu/thesaurus/definition/afferent/>
+#prefix BAMS: <http://brancusi1.usc.edu/RDF/thesaurusReference/>
 
+
+prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+prefix BAMS: <http://brancusi1.usc.edu/RDF/thesaurus>
+
+qres = g.query(
+     """SELECT DISTINCT ?x ?def
+       WHERE {
+          ?x BAMS:definition "Basal ganglia"^^xsd:string .
+          ?x BAMS:definition ?def.
+    	} LIMIT 20""")
+print(qres.result[0])
+print(qres.result[0])
+
+#for i in qres:
+#	print("Definition: %s" %qres.result[i])
+#Does not print, maybe need to append the node
+
+
+#for i in qres:
+#	print(qres.result[i])  
+#	print(qres.result[0])  	
+	
+    	
+    	
+print("printing results")
+print(qres.result[0][0])
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+Finally Catching on
+
+
+print("going to get results...")
+qres = g.query(
+     """SELECT ?subject ?object
+       WHERE {
+          ?subject "Basal ganglia"^^xsd:string  ?object.
+    	}""")
+print("printing results")
+print("The graph has " + str(len(g)) + " items in it")
+print("Name--not necessarily in string format: ")
+print(qres.result[1])
+print(qres.result[2])
+print(qres.result[3])
+print(qres.result[4])
+print(qres.result[5])
+print(qres.result[6])
+print(qres.result[7])
+print(qres.result[8])
+print(qres.result[9])
+print(qres.result[10])
+
+print(qres.result[11])
+print(qres.result[12])
+print(qres.result[13])
+print(qres.result[14])
+print(qres.result[15])
+print(qres.result[16])
+print(qres.result[17])
+print(qres.result[18])
+print(qres.result[19])
+print(qres.result[20])
+
+print(qres.result[21])
+print(qres.result[22])
+print(qres.result[23])
+print(qres.result[24])
+print(qres.result[25])
+print(qres.result[26])
+print(qres.result[27])
+print(qres.result[28])
+print(qres.result[29])
+print(qres.result[30])
+
+print(qres.result[31])
+print(qres.result[32])
+print(qres.result[33])
+print(qres.result[34])
+print(qres.result[35])
+print(qres.result[36])
+print(qres.result[37])
+print(qres.result[38])
+print(qres.result[39])
+print(qres.result[40])
+
+print(qres.result[41])
+print(qres.result[42])
+print(qres.result[43])
+print(qres.result[44])
+print(qres.result[45])
+print(qres.result[46])
+print(qres.result[47])
+print(qres.result[48])
+print(qres.result[49])
+print(qres.result[50])
+
+print(qres.result[51])
+print(qres.result[52])
+print(qres.result[53])
+print(qres.result[54])
+print(qres.result[55])
+print(qres.result[56])
+print(qres.result[57])
+print(qres.result[58])
+print(qres.result[59])
+print(qres.result[60])
+
+print(qres.result[61])
+print(qres.result[62])
+print(qres.result[63])
+print(qres.result[64])
+print(qres.result[65])
+print(qres.result[66])
+print(qres.result[67])
+print(qres.result[68])
+print(qres.result[69])
+print(qres.result[70])
+
+print(qres.result[71])
+print(qres.result[72])
+print(qres.result[73])
+print(qres.result[74])
+print(qres.result[75])
+print(qres.result[76])
+print(qres.result[77])
+print(qres.result[78])
+print(qres.result[79])
+print(qres.result[80])
+
+print(qres.result[81])
+print(qres.result[82])
+print(qres.result[83])
+print(qres.result[84])
+print(qres.result[85])
+print(qres.result[86])
+print(qres.result[87])
+print(qres.result[88])
+print(qres.result[89])
+print(qres.result[90])
+
+print(qres.result[91])
+print(qres.result[92])
+print(qres.result[93])
+print(qres.result[94])
+print(qres.result[95])
+print(qres.result[96])
+print(qres.result[97])
+	#Returned the following:
+	#(rdflib.term.URIRef(u'http://brancusi1.usc.edu/thesaurus/definition/corpora-quadrigemina/'), 
+	#rdflib.term.URIRef(u'http://brancusi1.usc.edu/RDF/definition'), 
+	#rdflib.term.BNode('Na7b3f149fb03438a89dd11a0a5143b17'))
+print(qres.result[98])
+	#Returned the following:
+	#(rdflib.term.URIRef(u'http://brancusi1.usc.edu/thesaurus/definition/corpora-quadrigemina/'), 
+	#rdflib.term.URIRef(u'http://brancusi1.usc.edu/RDF/definition'), 
+	#rdflib.term.BNode('N4a44966bbc4e47d0845c6dcb37a4bf08'))
+print(qres.result[99])
+	#Returned the following:
+	#(rdflib.term.URIRef(u'http://brancusi1.usc.edu/thesaurus/definition/corpora-quadrigemina/'), 
+	#rdflib.term.URIRef(u'http://brancusi1.usc.edu/RDF/definition'), 
+	#rdflib.term.BNode('N10adb95425cb49cc9138b3edcd66c9da'))
+print(qres.result[100])
+
+
+str(len(qres.result[0]))
+str(qres.result[0])
+##################################
+##################################
+qres = g.query(
+     """SELECT ?subject ?object
+       WHERE {
+        "Basal ganglia"^^xsd:string  ?object.
+    	}"""),
+str(qres.result[0])
+##################################
+##################################
+
+#########################################################################################
+#########################################################################################
 #Ask a specific question about that RDF document
 qres = g.query(
 	prefix bams: <http://brancusi1.usc.edu/RDF/>
@@ -326,6 +531,7 @@ qres = g.query(
     """SELECT DISTINCT ?b
        WHERE {
           ?a bams:Definition ?b .
+          ?x bams:Definition ?def.
        }""",
     #initNs=dict(
     #    property=rdflib.Namespace("http://neurolex.org/wiki/Special:URIResolver/Property-3A"),
@@ -334,3 +540,95 @@ qres = g.query(
 
 #print("Definition: %s" % qres.result[0])
 print(qres.result[0])	
+
+
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#Query Examples after reading the book (or most of it anyway)
+
+
+#Below is the model query:
+#########################################################################################
+#########################################################################################
+qres = g.query(
+     """SELECT ?subject ?predicate ?object
+       WHERE {
+          ?subject ?predicate ?object.
+    	} LIMIT 5""")
+
+print("printing results")
+
+#Search through everything
+#for i in qres:
+#	print("Definition: %s" %qres.result[i])
+
+#print("Name: %s" %qres.result[0])
+#TypeError: not all arguments converted during string formatting
+
+print("The graph has " + str(len(g)) + " items in it")
+
+print("Name--not necessarily in string format: ")
+print(qres.result[0])
+#########################################################################################
+#########################################################################################
+#cleaned up model query:
+qres = g.query(
+     """SELECT ?subject ?predicate ?object
+       WHERE {
+          ?subject ?predicate ?object.
+    	} LIMIT 5""")
+print(qres.result[0])
+#########################################################################################
+#########################################################################################
+#Building off that:
+
+
+#prefix bams: <http://brancusi1.usc.edu/RDF/>.
+qres = g.query(
+	#prefix bams: <http://brancusi1.usc.edu/RDF/>.
+     """SELECT ?subject ?predicate ?object
+       WHERE {
+          ?subject ?predicate ?object.
+    	} LIMIT 5""")
+print(qres.result[0])
+
+####
+
+#prefix bams: <http://brancusi1.usc.edu/RDF/>
+#prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>
+
+
+#########################################################################################
+#########################################################################################
+#########################################################################################
+#########################################################################################
+
+#Got rid of the indentation errors
+
+
+
+qres = g.query(
+""" SELECT ?subject ?predicate ?object
+	WHERE {
+		?subject ?predicate ?object.
+    } LIMIT 5""")
+
+print("printing results")
+
+#Search through everything
+#for i in qres:
+#	print("Definition: %s" %qres.result[i])
+
+#print("Name: %s" %qres.result[0])
+#TypeError: not all arguments converted during string formatting
+
+print("The graph has " + str(len(g)) + " items in it")
+
+print("Name--not necessarily in string format: ")
+print(qres.result[0])
