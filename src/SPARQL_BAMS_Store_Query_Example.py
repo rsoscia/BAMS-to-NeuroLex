e5ef209a9ee2a78667bc00732ef07d374a63197e
rsoscia <ryansoscia@rsoscia-res.resnet.ucsd.edu>
2013-10-16 21:14:31 -0700

    Storing RDF to a backend that persists and querying from it

--- src/SPARQL_BAMS_Store_Query_Example.py
+++ src/SPARQL_BAMS_Store_Query_Example.py
@@ -94,17 +94,127 @@ for r in qres.result:
     print("added on the object")
     print str(r[0]), str(r[1]), str(r[2])
 
+
+##############################################################################################
+##############################################################################################
 #Version 2:
 #Modified BAMS Query for fixed RDF:
 #Instead of this prefix:
 #http://brancusi1.usc.edu/thesaurus/
+#Using this prefix:
 #http://brancusi1.usc.edu/RDF/thesaurus
 
 
+PREFIX bams: <http://brancusi1.usc.edu/RDF/thesaurus/definition/>
 
 
 
+qres = g.query(
+     """PREFIX bams: <http://brancusi1.usc.edu/RDF/thesaurus/definition/>
+       SELECT ?predicate ?object
+       WHERE {
+          bams:Basal-ganglia/ ?predicate ?object.
+    	} LIMIT 5""")
 
+for r in qres.result:
+    #print str(r[0]), str(r[1])
+    
+    #added on the object
+    
+    print str(r[0]), str(r[1]), str(r[2])
+    
+#############################################################################################
+#Version 2:
+#Modified BAMS Query for fixed RDF:
+#Instead of this prefix:
+#http://brancusi1.usc.edu/thesaurus/
+#Using this prefix:
+#http://brancusi1.usc.edu/RDF/thesaurus
+http://brancusi1.usc.edu/RDF/thesaurusReference
+##################################################
+
+#Not sure if that's even the right description of the modification i'm about to make
+#Found the node though for the basal glanglia triple:
+#N8b46643d8653451da69a6a73576819d0
+
+#Now inserting it into a query:
+
+qres = g.query(
+     """SELECT ?predicate ?object
+       WHERE {
+          _:N8b46643d8653451da69a6a73576819d0 ?predicate ?object .
+    	} LIMIT 5""")
+
+for r in qres.result:
+    print str(r[0]), str(r[1])
+    
+#The above query returned the following:
+	#http://www.w3.org/1999/02/22-rdf-syntax-ns#type file:///anchor
+	#http://www.w3.org/1999/xlinkhref http://brancusi1.usc.edu/thesaurus/definition/tectum/
+	#http://www.w3.org/1999/xlinktype simple
+	#http://www.w3.org/1999/02/22-rdf-syntax-ns#type http://brancusi1.usc.edu/RDF/thesaurus
+	#http://brancusi1.usc.edu/RDF/definition N8b830f1c7f6c4002b3384eadd2196373
+#############################################################################################
+#############################################################################################
+#Changing the limit to something >5
+#N8b46643d8653451da69a6a73576819d0
+
+qres = g.query(
+     """SELECT ?predicate ?object
+       WHERE {
+          _:N8b46643d8653451da69a6a73576819d0 ?predicate ?object .
+    	} LIMIT 50""")
+
+for r in qres.result:
+    print str(r[0]), str(r[1])
+
+#############################################################################################
+#############################################################################################
+
+
+#Trying with a new ID
+Na88e23a883694760abef4476981f4573
+
+qres = g.query(
+     """SELECT ?predicate ?object
+       WHERE {
+          _:Na88e23a883694760abef4476981f4573 ?predicate ?object .
+    	} LIMIT 5""")
+
+for r in qres.result:
+    print str(r[0]), str(r[1])
+    
+#############################################################################################
+#############################################################################################
+#New Node:
+#N8b46643d8653451da69a6a73576819d0
+
+qres = g.query(
+     """SELECT ?predicate ?object
+       WHERE {
+          _:N8b46643d8653451da69a6a73576819d0 ?predicate ?object .
+    	} LIMIT 5""")
+
+for r in qres.result:
+    print str(r[0]), str(r[1])
+    
+qres = g.query(
+     """SELECT ?predicate ?object
+       WHERE {
+          ?predicate _:N8b46643d8653451da69a6a73576819d0 ?object .
+    	} LIMIT 5""")
+
+for r in qres.result:
+    print str(r[0]), str(r[1])
+    
+qres = g.query(
+     """SELECT ?predicate ?object
+       WHERE {
+          ?predicate ?object _:N8b46643d8653451da69a6a73576819d0 .
+    	} LIMIT 5""")
+
+for r in qres.result:
+    print str(r[0]), str(r[1])
 
 
 
